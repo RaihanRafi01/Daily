66b0fdb1c97c5283e50248280b31f279e3efdabd
Raihan <r.rafi.aiub@gmail.com>
2023-11-09 20:47:53 +0600

    todo 03

--- app/src/main/java/com/example/daily/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/daily/ui/home/HomeFragment.kt
@@ -1,12 +1,10 @@
 package com.example.daily.ui.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.daily.databinding.FragmentHomeBinding
